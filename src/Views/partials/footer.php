5b773ca766e988b01e791be44472a5e463c70ea5
roomno43 <studyxbsc@gmail.com>
2026-04-14 21:21:08 +0000

    final

--- src/Views/partials/footer.php
+++
@@ -1,3 +0,0 @@
-<script src="js/app.js" type="module"></script>
-</body>
-</html>
--- src/Views/partials/toast-container.php
+++
@@ -1,2 +0,0 @@
-<!-- ========== TOAST CONTAINER ========== -->
-<div class="toast-container" id="toast-container"></div>
